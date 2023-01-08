cc3cc89fd9291f67436e194bd10892b7e4e19b74
Ramon Rodrigues <ramonrodsouza@gmail.com>
2023-01-08 16:33:50 -0300

    configurando botão mobile

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var btnMobile = document.getElementById ("btn-mobile");
 
 function toggleMenu () {  //toggle
 
+    if (event.type === "touchstart") event.preventDefault();
+
     var nav = document.getElementById("nav-mobile");
-    
          nav.classList.toggle("active");
+
+    var active = nav.classList.contains ("active");
+            event.currentTarget.setAttribute("aria-expanded", active);
+
+    if (active) {
+
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    }
+
+    else {
+
+        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+
+    }
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);  //responde mais rápido no mobile
